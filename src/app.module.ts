fb82082924a0049c029541cdd44dbcabf9df1f1f
aslambabu <aslambabu@gmail.com>
2022-10-12 06:46:36 +0530

    pull p.b language details from api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,61 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 import { GraphQLModule } from '@nestjs/graphql';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CronService } from './cron/cron.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import { ProgressBibleLanguageDetailsService } from './progress_bible_language_details/progress_bible_language_details.service';
+import { ProgressBibleLanguageDetailModule } from './progress_bible_language_details/progress_bible_language_details.module';
+import { ProgressBibleLanguageDetail } from './model';
+
 
 @Module({
   imports: [
+    ProgressBibleLanguageDetailModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [ ProgressBibleLanguageDetail ],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
+      sortSchema: true
     }),
     CoreModule,
+    HttpModule,
+    ScheduleModule.forRoot()
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CronService],
+  // entities: [ProgressBibleLanguageDetail],
+  exports: [
+
+  ]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private readonly config: ConfigService){
+    console.log("db host")
+    console.log(config.get('DB_HOST'))
+    console.log(config.get('DB_PORT'))
+    console.log(config.get('DB_USERNAME'))
+    console.log(config.get('DB_PASSWORD'))
+    console.log(config.get('DB_NAME'))
+  }
+}
---
+++ src/progress_bible_language_details/progress_bible_language_details.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProgressBibleLanguageDetail } from '../model/progress_bible_language_detail.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProgressBibleLanguageDetailsService {
+  constructor(@InjectRepository(ProgressBibleLanguageDetail) private readonly repo: Repository<ProgressBibleLanguageDetail>) { }
+
+  public async getAll() {
+    return await this.repo.find();
+  }
+
+  public async isExist(unit_code: string, primary_country_code: string ){
+    return await this.repo.count({
+        where: {
+            unit_code: unit_code,
+            primary_country_code: primary_country_code
+        }
+    })
+  }
+
+
+  public async insert(data: any){
+    return await this.repo.insert({
+        unit_code: data.unit_code,
+        unit_type: data.unit_type,
+        unit_name: data.unit_name,
+        unit_full_name: data.unit_full_name,
+        ethnologue_name: data.ethnologue_name,
+        iso_639_3_code: data.iso_639_3_code,
+        is_sign_language: data.is_sign_language,
+        code_status: data.code_status,
+        language_status: data.language_status,
+        language_scope: data.language_scope,
+        primary_continent: data.primary_continent,
+        primary_country_name: data.primary_country_name,
+        primary_country_code: data.primary_country_code,
+        retirement_explanation: data.retirement_explanation,
+        how_to_fix: data.how_to_fix,
+        retired_date: data.retired_date,
+        show_active_language: data.show_active_language,
+        show_retired_language: data.show_retired_language,
+        show_active_dialect: data.show_active_dialect,
+        show_retired_dialect: data.show_retired_dialect,
+        show_sign_language: data.show_sign_language
+    })
+  }
+
+
+}
