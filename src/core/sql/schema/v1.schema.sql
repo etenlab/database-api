9d98ea4452d426abdeba115c97fd3f602aa456ae
hiroshi <hiroshi.tan57@gmail.com>
2023-01-06 13:40:53 +0000

    Remove discussion & notification procedures

--- src/core/sql/schema/v1.schema.sql
+++ src/core/sql/schema/v1.schema.sql
@@ -299,27 +299,6 @@ create table admin.discussions (
   row bigint not null
 );
 
-create or replace function admin.fn_discussion_created() 
-	returns trigger as $discussion_created$
-	begin
-		perform pg_notify(
-    		'discussion_created',
-			json_build_object(
-			    'operation', TG_OP,
-			    'record', row_to_json(NEW)
-    		)::text);
-  		return NEW;
-	end;
-	$discussion_created$ language plpgsql;
-
-drop trigger if exists discussion_created
-  on admin.discussions;
- 
-create trigger discussion_created
-  after insert 
-  on admin.discussions
-  for each row execute function admin.fn_discussion_created();
-
 create table admin.posts (
   id bigserial primary key,
   discussion_id bigint references admin.discussions(id),
@@ -341,35 +320,6 @@ create table admin.posts (
 
 create index posts_search_gin on admin.posts using gin (search_text);
 
-create or replace function admin.fn_post_changed() 
-	returns trigger as $post_changed$
-  declare
-    row RECORD;
-	begin
-    IF (TG_OP = 'DELETE') THEN
-      row = OLD;
-    ELSE 
-      row = NEW;
-    END IF;
-		
-    perform pg_notify(
-    		'post_changed',
-			json_build_object(
-			    'operation', TG_OP,
-			    'record', row_to_json(row)
-    		)::text);
-  		return row;
-	end;
-	$post_changed$ language plpgsql;
-
-drop trigger if exists post_changed
-  on admin.posts;
- 
-create trigger post_changed
-  after insert or update or delete
-  on admin.posts
-  for each row execute function admin.fn_post_changed();
-
 create table admin.reactions (
   id bigserial primary key,
   user_id bigint not null references admin.users(user_id), 
@@ -378,35 +328,6 @@ create table admin.reactions (
   content varchar(64) not null,
   unique (user_id, content, post_id)
 );
-
-create or replace function admin.fn_reaction_changed() 
-	returns trigger as $reaction_changed$
-	declare
-    row RECORD;
-	begin
-    IF (TG_OP = 'DELETE') THEN
-      row = OLD;
-    ELSE 
-      row = NEW;
-    END IF;
-
-		perform pg_notify(
-    		'reaction_changed',
-			json_build_object(
-			    'operation', TG_OP,
-			    'record', row_to_json(row)
-    		)::text);
-  		return row;
-	end;
-	$reaction_changed$ language plpgsql;
-
-drop trigger if exists reaction_changed
-  on admin.reactions;
- 
-create trigger reaction_changed
-  after insert or update or delete
-  on admin.reactions
-  for each row execute function admin.fn_reaction_changed();
 
 -- file ---------------------------------------------------
 create table admin.files (
@@ -424,35 +345,6 @@ create table admin.relationship_post_file (
   file_id bigint not null references admin.files(id)
 );
 
-create or replace function admin.fn_relationship_post_file_deleted() 
-	returns trigger as $relationship_post_file_deleted$
-	declare
-    row RECORD;
-	begin
-    IF (TG_OP = 'DELETE') THEN
-      row = OLD;
-    ELSE 
-      row = NEW;
-    END IF;
-
-		perform pg_notify(
-    		'relationship_post_file_deleted',
-			json_build_object(
-			    'operation', TG_OP,
-			    'record', row_to_json(row)
-    		)::text);
-  		return row;
-	end;
-	$relationship_post_file_deleted$ language plpgsql;
-
-drop trigger if exists relationship_post_file_deleted
-  on admin.relationship_post_file;
- 
-create trigger relationship_post_file_deleted
-  after delete
-  on admin.relationship_post_file
-  for each row execute function admin.fn_relationship_post_file_deleted();
-
 -- NOTIFICATIONS ----------------------------------------------------
 create table admin.notifications (
   id bigserial primary key,
@@ -463,27 +355,6 @@ create table admin.notifications (
   content text,
   created_at timestamp default current_timestamp
 );
-
-create or replace function admin.fn_notification_created() 
-	returns trigger as $notification_created$
-	begin
-		perform pg_notify(
-    		'notification_created',
-			json_build_object(
-			    'operation', TG_OP,
-			    'record', row_to_json(NEW)
-    		)::text);
-  		return NEW;
-	end;
-	$notification_created$ language plpgsql;
-
-drop trigger if exists notification_created
-  on admin.notifications;
- 
-create trigger notification_created
-  after insert
-  on admin.notifications
-  for each row execute function admin.fn_notification_created();
 
 -- DATASETS ---------------------------------------------------------
 
