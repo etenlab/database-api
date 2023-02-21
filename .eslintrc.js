aa0940ecbc44b749c9f29d7f6fbad857556380e3
cwshkut <alexey.shkut@clockwise.software>
2023-02-21 12:28:58 +0000

    fix linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,14 +21,13 @@ module.exports = {
         // '@typescript-eslint/explicit-function-return-type': 'off',
         // '@typescript-eslint/explicit-module-boundary-types': 'off',
         // '@typescript-eslint/no-explicit-any': 'off',
-        "semi": "on",
+        "semi": 2,
         // "@typescript-eslint/semi": ["error"],
         'prettier/prettier': [
             'error',
             {
                 singleQuote: true,
-                semi: true,
-                trailingComma: 'none',
+                trailingComma: 'all',
             }
         ],
     },
